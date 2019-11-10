4544903d6b53f0e526c7df015358de515cebb4aa
moyumin <347307741@qq.com>
2019-11-10 22:49:05 +0800

    add

---
+++ debug.js
@@ -0,0 +1,10 @@
+const webpack = require("./lib/webpack.js");
+ const config = require('./webpack.config.js');//1.读取配置文件
+ const compiler = webpack(config);
+ function compilerCallback(err, stats) {
+     const statsString = stats.toString();
+     console.log(statsString);
+ }
+ compiler.run((err,stats)=>{
+     compilerCallback(err, stats);
+ });
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+module.exports = {
+    mode:'development',
+    context: path.resolve(__dirname),
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    }
+}
